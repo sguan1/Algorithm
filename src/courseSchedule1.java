da7ae62b16a56d1e176db58904564ab858f89967
sguan1 <sguan1@andrew.cmu.edu>
2018-01-29 16:14:12 -0500

    Optimize the time complexity

--- src/courseSchedule1.java
+++ src/courseSchedule1.java
@@ -5,43 +5,38 @@ import java.util.*;
  */
 public class courseSchedule1 {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> indegree = new HashMap<>();
+        Map<Integer, Integer> indegree = new HashMap<>();
         Map<Integer, List<Integer>> outdegree = new HashMap<>();
+        for (int i = 0; i < numCourses; i++) {
+            indegree.put(i, 0);
+            outdegree.put(i, new ArrayList<>());
+        }
         for (int[] prerequisite : prerequisites) {
             int pre = prerequisite[0];
             int post = prerequisite[1];
-            if (!indegree.containsKey(pre)) {
-                indegree.put(pre, new ArrayList<>());
-                outdegree.put(pre, new ArrayList<>());
-            }
-            if (!indegree.containsKey(post)) {
-                indegree.put(post, new ArrayList<>());
-                outdegree.put(post, new ArrayList<>());
-            }
-            indegree.get(post).add(pre);
+            indegree.put(post, indegree.get(post) + 1);
             outdegree.get(pre).add(post);
         }
-        //start from the course with zero ingree
         Queue<Integer> queue = new LinkedList<>();
         for (int key : indegree.keySet()) {
-            if (indegree.get(key).size() == 0) {
+            if (indegree.get(key) == 0) {
                 queue.offer(key);
             }
         }
-        Set<Integer> set = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
         while (!queue.isEmpty()) {
             for (int i = 0; i < queue.size(); i++) {
                 Integer curr = queue.poll();
-                set.add(curr);
+                list.add(curr);
                 List<Integer> nexts = outdegree.get(curr);
                 for (int next : nexts) {
-                    indegree.get(next).remove(curr);
-                    if (indegree.get(next).size() == 0) {
+                    indegree.put(next, indegree.get(next) - 1);
+                    if (indegree.get(next) == 0) {
                         queue.offer(next);
                     }
                 }
             }
         }
-        return set.size() == indegree.size();
+        return list.size() == numCourses;
     }
 }
